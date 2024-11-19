7978e5ceab4dc4c17c425afaf70fe8122232a057
SebastianMaldonado <sebastianmaldonado@uninorte.edu.co>
2024-11-19 13:03:49 -0500

    Django User CRUD

---
+++ softwarebackend/usercrud/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from django.urls import path, include
+from .views import UserProfileViewSet
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'user', UserProfileViewSet, basename='Image')
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
+]
+
