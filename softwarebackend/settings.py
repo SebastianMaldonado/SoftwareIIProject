7822e7722495eb2bd069550418da01123ab1dbca
SebastianMaldonado <sebastianmaldonado@uninorte.edu.co>
2024-11-24 15:11:09 -0500

    all host allowed

--- softwarebackend/settings.py
+++ softwarebackend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-rysv@*7k*6pfjfdst-aduu3utpk6thn@l*q#!i4%$*ng-q7)b0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
